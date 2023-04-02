4ca48f23fc35d444ddb9444661b3afcd70d9a226
Emmy01Git <amitsku.1994@gmail.com>
2023-04-02 23:59:52 +0530

    updated

--- dom.js
+++ dom.js
@@ -4,3 +4,14 @@ secondItem.style.backgroundColor = 'green';
 
 var thirdItem = document.querySelector('.list-group-item:nth-child(3)')
 thirdItem.style.color = '#FFFFFF';
+
+//QUERYSELECTORALL
+var titles = document.querySelectorAll('.title');
+//console.log(titles;)
+//titles[3].style.backgroundColor = 'yellow';
+
+var odd = document.querySelectorAll('li:nth-child(odd)');
+
+for(var i = 0; i < odd.length; i++){
+    odd[i].style.backgroundColor = 'blue';
+}
